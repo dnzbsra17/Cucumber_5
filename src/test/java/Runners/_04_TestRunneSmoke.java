b97f89466bb93ac3eed9e5734563aea80313817e
busrakuzucu <busrakuzucu94@gmail.com>
2023-05-02 20:05:06 +0300

    aa

--- src/test/java/Runners/_04_TestRunneSmoke.java
+++ src/test/java/Runners/_04_TestRunneSmoke.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         tags = "@SmokeTest", // tüm yazılan testler içerisinde smoke test olanları çalıştır demek
         features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"}
-
+        glue = {"StepDefinitions"},
+        plugin =  {"html:target/site/cucumber-pretty.html"} //basit rapor
 
 )
 public class _04_TestRunneSmoke extends AbstractTestNGCucumberTests {
